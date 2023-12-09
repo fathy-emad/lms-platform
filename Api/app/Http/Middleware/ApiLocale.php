963433cc46c0051e3ab4803a7ab0d52d20768a00
fathy-emad <fatyemad@gmail.com>
2023-12-09 22:00:45 +0200

    add lang middleware and model

---
+++ Api/app/Http/Middleware/ApiLocale.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Locale;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response) $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->hasHeader('locale'))
+        {
+            $locale = $request->header('locale');
+            $exists_locale = Locale::where('locale', $locale)->first();
+
+            if ($exists_locale) App::setLocale($locale);
+        }
+
+        return $next($request);
+    }
+}
