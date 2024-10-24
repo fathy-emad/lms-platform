4f3c486aaba71e88589879c20226882952fb14e2
fathy-emad <fatyemad@gmail.com>
2024-10-24 22:28:01 +0300

    commit

--- resources/views/website_layouts/home_sections/category.blade.php
+++ resources/views/website_layouts/home_sections/category.blade.php
@@ -20,17 +20,17 @@
         </div>
         <div class="owl-carousel mentoring-course owl-theme aos" data-aos="fade-up">
             @foreach($years as $year)
-                <div class="feature-box text-center ">
-                    <div class="feature-bg">
+                <div class="feature-box text-center">
+                    <div class="feature-bg p-1">
                         <div class="feature-header">
-                            <div class="feature-icon">
+                            <div class="feature-icon" style="margin: 0 !important; width: 100%; height: 100%">
                                 <a href="{{ route("student.curricula", ["year_id" => $year->id]) }}">
-                                    <img src="{{ URL::asset(isset($year->image["file"]) ? 'uploads/' . $year->image["file"] : '/build/img/categories-icon.png') }}" alt="Img">
+                                    <img class="rounded rounded-top-2" src="{{ URL::asset(isset($year->image["file"]) ? 'uploads/' . $year->image["file"] : '/build/img/categories-icon.png') }}" alt="Img">
                                 </a>
                             </div>
-                            <div class="feature-cont">
-                                <div class="feature-text">
-                                    <a href="{{ route("student.curricula", ["year_id" => $year->id]) }}">
+                            <div class="feature-cont mt-2" style="margin: 0 !important; width: 100%">
+                                <div class="feature-text p-0 m-0 mt-2" style="width: 100%; max-width: unset">
+                                    <a style="width: 100%" href="{{ route("student.curricula", ["year_id" => $year->id]) }}">
                                         {{ $year->yearTranslate->translates[app()->getLocale()] }}
                                     </a>
                                 </div>
